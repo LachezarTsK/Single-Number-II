f1e5b4fd1da46ae06382e9f91e17f4886704c860
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-15 11:29:27 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,21 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+
+    int singleNumber(vector<int>& nums) {
+
+        int occuredOnce = 0;
+        int occuredTwice = 0;
+
+        for (int n : nums) {
+            occuredOnce = (~occuredTwice) & (occuredOnce ^ n);
+            occuredTwice = (~occuredOnce) & (occuredTwice ^ n);
+        }
+
+        return occuredOnce;
+    }
+};
